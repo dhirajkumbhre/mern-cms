8d4cba5fca9e0eb62e418ff2bbc997cda6001ee9
Dhiraj Kumbhre <131527754+dhirajkumbhre@users.noreply.github.com>
2025-11-27 06:09:37 +0530

    Refactor App component with BrowserRouter and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,41 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import PostEditor from "./pages/PostEditor";
 import PostView from "./pages/PostView";
 import Auth from "./pages/Auth";
-import ProtectedRoute from "./ProtectedRoute";
+import ProtectedRoute from "./ProtectedRoute"; // keep your ProtectedRoute
 
-export default function App() {
+export default function App(){
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <BrowserRouter>
       <Navbar />
-
-      <Routes>
-        <Route path="/" element={<Home />} />
-
-        <Route
-          path="/dashboard"
-          element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route
-          path="/editor"
-          element={
-            <ProtectedRoute>
-              <PostEditor />
-            </ProtectedRoute>
-          }
-        />
-
-        <Route path="/post/:id" element={<PostView />} />
-        <Route path="/auth" element={<Auth />} />
-      </Routes>
-    </div>
+      <div className="pt-16"> {/* offset for fixed navbar */}
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/post/:id" element={<PostView />} />
+          <Route path="/editor" element={<ProtectedRoute><PostEditor /></ProtectedRoute>} />
+          <Route path="/editor/:id" element={<ProtectedRoute><PostEditor /></ProtectedRoute>} />
+          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }

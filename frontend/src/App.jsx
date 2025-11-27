4b074b2fb8ba6c455bca29b666c698941798aa24
Dhiraj Kumbhre <131527754+dhirajkumbhre@users.noreply.github.com>
2025-11-27 06:19:48 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,25 @@
-import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import Dashboard from "./pages/Dashboard";
 import PostEditor from "./pages/PostEditor";
 import PostView from "./pages/PostView";
 import Auth from "./pages/Auth";
-import ProtectedRoute from "./ProtectedRoute"; // keep your ProtectedRoute
+import ProtectedRoute from "./ProtectedRoute";
 
-export default function App(){
+export default function App() {
   return (
-    <BrowserRouter>
+    <div className="pt-16">
       <Navbar />
-      <div className="pt-16"> {/* offset for fixed navbar */}
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/auth" element={<Auth />} />
-          <Route path="/post/:id" element={<PostView />} />
-          <Route path="/editor" element={<ProtectedRoute><PostEditor /></ProtectedRoute>} />
-          <Route path="/editor/:id" element={<ProtectedRoute><PostEditor /></ProtectedRoute>} />
-          <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/auth" element={<Auth />} />
+        <Route path="/dashboard" element={<ProtectedRoute><Dashboard /></ProtectedRoute>} />
+        <Route path="/editor" element={<ProtectedRoute><PostEditor /></ProtectedRoute>} />
+        <Route path="/editor/:id" element={<ProtectedRoute><PostEditor /></ProtectedRoute>} />
+        <Route path="/post/:id" element={<PostView />} />
+      </Routes>
+    </div>
   );
 }
+
